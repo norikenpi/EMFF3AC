b0cc8d1b585fcaa735848ce0895c5c4d2b85002d
norikenpi <norikenpi@gmail.com>
2023-07-19 09:05:25 +0900

    feat:For easier review later, add the date to the plotConvergePlot.

--- Show/plotConvergePlot.m
+++ Show/plotConvergePlot.m
@@ -18,9 +18,14 @@ function plotConvergePlot(array, first_seed, type, param)
     ylim([-2000, 2000])
     ylabel('Converge time(s)');
     title(sprintf('Plot of Converge time(2cm) %s', type));
+    %グラフ画像に日付を入れる。
+    xPosition = xlim; % x軸の範囲を取得
+    yPosition = ylim*0.8; % y軸の範囲を取得
+    text(xPosition(2), yPosition(2), string(param.date), 'HorizontalAlignment', 'right', 'VerticalAlignment', 'bottom');
+
     grid on
     hold off
-    
+
     mkdir(param.path_data)
     filename_fig = strcat(param.path_data, sprintf('/%s.fig', type));
     filename_jpg = strcat(param.path_data, sprintf('/%s.jpg', type));
