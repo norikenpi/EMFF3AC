520b50f64850b07c04021e8184bfe132729d9148
norikenpi <norikenpi@gmail.com>
2023-09-03 22:44:38 +0900

    add three satellite vertsion.

--- startSimulation.m
+++ startSimulation.m
@@ -4,7 +4,7 @@
 param = setSimulationParameters();
 %param.t = 2000; 
 %param.freq_all = false;
-param.j = 1;
+param.j = 7;
 param.pair_type = "Takahashi";
 param.pair_type = "velocity";
 simulateSatellite(param);
